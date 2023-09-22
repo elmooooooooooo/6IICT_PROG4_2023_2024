94dc220bb7e9b22106551a7b83118ff51f2ba589
elmooooooooooo <mohcine.elouahabi@gmail.com>
2023-09-22 20:11:49 +0200

    hvkyvyt

--- hfst_2/oefenmee/oefenmee_5.py
+++ hfst_2/oefenmee/oefenmee_5.py
@@ -1,11 +1,13 @@
 # Pas de URL aan zoals aangegeven in de verschillende niveau's.
 import requests
 import json
-
-url = "https://v2.jokeapi.dev/joke/Christmas"
+counter = 0
+url = "https://v2.jokeapi.dev/joke/Christmas?a"
 response_json = requests.get(url).json() # Haal JSON uit response.
-
-with open("hfst_2/oefenmee/bericht.kerstjoke.json", "w") as fp:
+while True:
+   counter = counter + 1
+   str(counter)
+   with open(f"hfst_2/oefenmee/hoppa{counter}.json", "w") as fp:
     json.dump(response_json, fp)
     # Bepaal of de grap uit 1 of 2 delen bestaat.
     if ("joke" in response_json):
