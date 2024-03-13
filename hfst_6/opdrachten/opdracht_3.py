55c7d3aff1cfeb72217cac4e80af241d94e16774
elmooooooooooo <mohcine.elouahabi@gmail.com>
2024-03-13 10:01:06 +0100

    opdracht 3, 4

--- hfst_6/opdrachten/opdracht_3.py
+++ hfst_6/opdrachten/opdracht_3.py
@@ -1,4 +1,22 @@
 # Bepaal recursief de grootte van een map.
+import os
+
+def grootte_map(pad):
+    items = os.listdir(pad)  # Zet de inhoud van de map in een lijst.
+    grootte = 0
+
+    for item in items: # Overloop ieder item.
+        item_pad = os.path.join(pad, item)  # Haal pad van item op.
+
+        if os.path.isfile(item_pad):        # Is item een bestand?
+            #grootte = os.path.getsize(item_pad) / (1024**2) + grootte # Grootte in MB.
+            print(f"{item} is een bestand.")
+        elif os.path.isdir(item_pad):       # Is item een map?
+            # grootte van een map is verwaarloosbaar.
+            grootte = os.path.getsize(item_pad) / (1024**2) + grootte # Grootte in MB.
+            #print(f"{item} is een map.")
+
+    return grootte
 
 
 
--- hfst_6/opdrachten/opdracht_4.py
+++ hfst_6/opdrachten/opdracht_4.py
@@ -1,7 +1,14 @@
 # Bepaal recursief of een getal voldoet aan het luhn-criteria.
 
-
-
+def luhn(reeks):
+    reeks = str(reeks)
+    if len(reeks) == 0:
+        return 0
+    
+    getal = int(reeks[0])
+    if len(reeks)%2 == 0: getal *= 2
+    if getal > 9: getal -= 9
+    return getal + luhn(reeks[1:])
 
 print( luhn(4687612962034330) ) # 70 --> geldig
 print( luhn(6728003096702784) ) # 70 --> geldig
