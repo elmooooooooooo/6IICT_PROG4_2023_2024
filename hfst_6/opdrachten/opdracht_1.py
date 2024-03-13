631ac2953abc14c66eab78a075d7c1defff5b797
elmooooooooooo <mohcine.elouahabi@gmail.com>
2024-03-13 09:43:22 +0100

    opdracht 1-2

--- hfst_6/opdrachten/opdracht_1.py
+++ hfst_6/opdrachten/opdracht_1.py
@@ -1,7 +1,11 @@
 # Bepaal het getal van fibonnaci recursief.
+def fib(getal):
+    if getal == 1 or getal == 2:
+        return 1
+    else:
+        getal = fib(getal - 1) + fib(getal - 2)
 
-
-
+    return getal
 
 print( fib(2) )     # 1
 print( fib(4) )     # 3
--- hfst_6/opdrachten/opdracht_2.py
+++ hfst_6/opdrachten/opdracht_2.py
@@ -1,6 +1,16 @@
 # Bepaal recursief of een woord een palindroom is.
+def palindroom(woord):
 
+    if len(woord) == 1 or len(woord) == 0:
+        return True
+    
+    laatste = woord[-1] 
+    eerste = woord[0]
 
+    if laatste == eerste:
+        return palindroom(woord[1:-1])
+    else:
+        return False
 
 
 print( palindroom("lepel") ) # True
