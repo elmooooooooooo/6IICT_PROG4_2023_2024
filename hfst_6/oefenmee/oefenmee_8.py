d3f5d1c82b26ac53a93cfe060b4676f6ff63efa0
elmooooooooooo <mohcine.elouahabi@gmail.com>
2024-03-13 09:05:24 +0100

    oefenmees klaar

--- hfst_6/oefenmee/oefenmee_8.py
+++ hfst_6/oefenmee/oefenmee_8.py
@@ -1,6 +1,13 @@
 # Bepaal de som van alle elementen (en sub-elementen) in een (geneste) lijst.
-def som_lijst():
-    return
+def som_lijst(lijst):
+    aantal = 0
+    for element in lijst:
+        if type(element) != int:
+            aantal = aantal + som_lijst(element)
+        else:
+            aantal += element   
+    return aantal
+
 
 print( som_lijst([1,2,3,4]) )           # 10
 print( som_lijst([1, [2,3], 4]) )       # 10
--- hfst_6/oefenmee/oefenmee_9.py
+++ hfst_6/oefenmee/oefenmee_9.py
@@ -1,6 +1,21 @@
 # Bepaal het aantal bestanden in een map (en alle submappen).
+import os
 
+def doorzoek_map(pad):
+    items = os.listdir(pad)  # Zet de inhoud van de map in een lijst.
 
+    teller = 0
+    for item in items: # Overloop ieder item.
+        item_pad = os.path.join(pad, item)  # Haal pad van item op.
+
+        if os.path.isfile(item_pad):        # Is item een bestand?
+            teller = teller + 1
+            # print(f"{item} is een bestand.")
+        elif os.path.isdir(item_pad):       # Is item een map?
+            # print(f"{item} is een map.")
+            teller = doorzoek_map(item_pad) + teller
+
+    return teller
 
 " Opgelet! uitkomsten geven aantal bestanden aan. Mappen zelf zijn niet meegeteld. "
 print( doorzoek_map("hfst_1") )    # 40 
